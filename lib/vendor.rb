1f23702f9ee6400478536a0aba9ae16cf460b5e9
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-20 09:31:50 -0700

    Write code to stock into inventory and check stock

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -7,4 +7,20 @@ class Vendor
     @inventory ={}
   end
 
+  def check_stock(item)
+    if @inventory[item].nil?
+      @inventory[item] = 0
+    else
+      @inventory[item]
+    end
+  end
+
+  def stock(item, quantity)
+    if @inventory[item].nil?
+      @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
+  end
+
 end

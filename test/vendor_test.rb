943b030947c44b805521cbc29841ade47ec2af63
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-20 09:29:24 -0700

    Add additional assertions to stock test, write inventory test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -20,22 +20,22 @@ class VendorTest < Minitest::Test
   def test_it_can_stock
     assert_equal 0, @vendor.check_stock(@item1)
     @vendor.stock(@item1, 30)
+    assert_equal 30, @vendor.check_stock(@item1)
+    @vendor.stock(@item1, 20)
+    assert_equal 50, @vendor.check_stock(@item1)
   end
 
-  def test_it_has_inventor
-  # assert_equal  @vendor.inventory
-  #   #=> {#<Item:0x007f9c56740d48...> => 30}
-  #
-  #   @vendor.check_stock(item1)
-  #   #=> 30
-  #
-  #   @vendor.stock(item1, 25)
-  #
-  #   @vendor.check_stock(item1)
-  #   #=> 55
-  #
-  #   @vendor.stock(item2, 12)
+  def test_it_stores_inventory_as_hash
+    @vendor.stock(@item1, 30)
+    expected = {@item1 => 30}
 
+    assert_equal expected, @vendor.inventory
+
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+
+    expected = {@item1 => 55, @item2 => 12}
+    assert_equal expected, @vendor.inventory
   end
 
 end
